e8018f2eead0eb1f23b78e504d4809262f853a67
SharathChampzz <54370770+SharathChampzz@users.noreply.github.com>
2020-05-19 18:19:12 +0530

    Added Comments

--- MergePDFs.py
+++ MergePDFs.py
@@ -1,25 +1,18 @@
-from PyPDF2 import PdfFileMerger, PdfFileReader
+#To merge PDF files in a same directory in order
+from PyPDF2 import PdfFileMerger, PdfFileReader    #pip install PyPDF2
 import os
-# Call the PdfFileMerger
-mergedObject = PdfFileMerger()
- 
-dir = r"C:\Users\SHARATH KUMAR H K\Desktop\Projects\DC\event2"
 
-# Loop through all of the pdf in directory and append their pages
-for pdf in os.listdir(dir):
- 	PdfName = dir + '\\' + pdf
+mergedObject = PdfFileMerger() # create the PdfFileMerger object
+ 
+dir = r"C:\Users\SHARATH KUMAR H K\Desktop\Projects\DC\event2"   # Directory location of PDF files
+
+for pdf in os.listdir(dir): 					# Loop through all of the pdf in directory and append their pages
+ 	PdfName = dir + '\\' + pdf  				# Getting Full path for PDF
  	mergedObject.append(PdfFileReader(PdfName, 'rb'))
 
-# I had 116 files in the folder that had to be merged into a single document
-
-#for fileNumber in range(1, 117):
-    
- 
 # Write all the files into a file which is named as shown below
 OutputName = input('Enter Output PDF Name : ')
 if OutputName.endswith('.pdf') is False:  # No extension found
-	OutputName = OutputName + '.pdf'
+	OutputName = OutputName + '.pdf'  # so add extension by yourself
 mergedObject.write(OutputName)
-
 print(f'Saved! PDF at {os.getcwd()}')
-print('Done!')
